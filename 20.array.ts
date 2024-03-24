111175d0caffa0cbf2b2cd00a91ec7dd23e9a931
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-24 22:10:57 +0500

    Add files via upload

---
+++ 20.array.ts
@@ -0,0 +1,16 @@
+
+// Write a program that creates a list containing  items.
+
+let cities:string[]=["karachi","lahore","islamabad","multan"];
+console.log(cities);
+
+
+// lets add some more countries in array using push method.
+cities.push("quetta");
+cities.push("hyderabad");
+
+console.log(cities);
+
+
+
+
