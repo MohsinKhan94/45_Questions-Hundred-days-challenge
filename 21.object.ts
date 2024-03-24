58e978a11ff00c9b63fef2373f99f61bb7dfdd77
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-24 22:13:29 +0500

    Add files via upload

---
+++ 21.object.ts
@@ -0,0 +1,17 @@
+// Write a program that creates Objects containing these items.
+
+let student:student = {
+    name :"mohsin khan",
+    age : 18,
+    ispass:true,
+    nationality:"pakistani",
+
+}
+
+interface student{
+    name:string,
+    age:number,
+    ispass:boolean,
+    nationality:string,
+}
+console.log(student);
