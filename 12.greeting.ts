5b0df0b1d4167c04221e20f9ced06a7e3d00e0bf
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-24 21:59:23 +0500

    Add files via upload

---
+++ 12.greeting.ts
@@ -0,0 +1,7 @@
+let friendsName:string[] = ['MURSALEEN','AHMED','ALI','ADNAN'];
+let message:string = 'how are you';
+
+for(let i=0; i<friendsName.length; i++){
+    console.log(message + friendsName [i]) ;
+    
+}
