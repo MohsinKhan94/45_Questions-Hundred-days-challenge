03a2519b3ab8afef80c5f7b73647792c4ebc599d
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:28:05 +0500

    Add files via upload

---
+++ 39.cities-names.ts
@@ -0,0 +1,11 @@
+
+
+function  city_country(city:string,country:string){
+    return `${city},${country}`;
+}
+
+// Calling the function with three city-country pairs
+
+console.log(city_country("lahore","pakistan"));
+console.log(city_country("Paris", "France"));
+console.log(city_country("New York", "USA"));
