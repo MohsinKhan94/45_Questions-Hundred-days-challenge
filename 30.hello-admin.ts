1ebd3191e43a1edcafc134f89054c10b8f1ef85f
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:20:43 +0500

    Add files via upload

---
+++ 30.hello-admin.ts
@@ -0,0 +1,13 @@
+
+const usernames: string[] = ['admin', 'eric', 'john', 'alice', 'emma'];
+
+for (const username of usernames) {
+    if (username === 'admin') {
+        console.log("Hello admin, would you like to see a status report?");
+    } else {
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    }
+}
+
+
+
