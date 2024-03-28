7877f3814b428af1f72fbec932e3648b9d2f9041
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:18:23 +0500

    Add files via upload

---
+++ 26.alien-color2.ts
@@ -0,0 +1,20 @@
+// Version that runs the if block:
+
+let alien_color = "green";
+
+if(alien_color == "green"){
+    console.log("you just earned 5 points for shooting the alien")
+}else{
+    console.log("you just earned 10 points for shooting the alien")
+}
+
+
+// Version that runs the else block:
+
+ alien_color = "red";
+
+if(alien_color == "green"){
+    console.log("you just earned 5 points for shooting the alien")
+}else{
+    console.log("you just earned 10 points for shooting the alien")
+}
