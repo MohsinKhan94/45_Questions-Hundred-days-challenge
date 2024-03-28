0f440946b0086157681c47c82e504bfeee9cf43b
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:20:09 +0500

    Add files via upload

---
+++ 29.favorite-fruit.ts
@@ -0,0 +1,22 @@
+
+let favorite_fruits:string[] = ["apple","banana","orange"];
+
+if(favorite_fruits.includes("apple")){
+    console.log("i really like apple")
+}
+
+if(favorite_fruits.includes("banana")){
+    console.log("i really like banana")
+}
+
+if(favorite_fruits.includes("orange")){
+    console.log("i really like orange")
+}
+
+if(favorite_fruits.includes("mango")){
+    console.log("i really like mango")
+}
+
+if(favorite_fruits.includes("cherries")){
+    console.log("i really like cherries")
+}
