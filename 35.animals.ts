cc5decc6622f0efe9a5e666dc074778dea210113
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:25:10 +0500

    Add files via upload

---
+++ 35.animals.ts
@@ -0,0 +1,11 @@
+
+let animals:string[] = ["lion","tiger","leopards"];
+
+animals.forEach(animals => {
+    console.log(`A ${animals}  would make a great pet`);
+});
+
+console.log("They have sharp claws and teeth for hunting.")
+
+console.log("Any of these animals would make a great pet!")
+
