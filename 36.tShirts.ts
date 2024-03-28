169e635405e3b16820b35299b5e3952e6b4affd0
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:26:08 +0500

    Add files via upload

---
+++ 36.tShirts.ts
@@ -0,0 +1,8 @@
+
+function make_shirt(size:string, text:string){
+console.log(`printing shirt with ${size} size and text: "${text}" `);
+}
+
+// calling the function
+
+make_shirt("large","hello world");
