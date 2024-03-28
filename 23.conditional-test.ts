066157189341bdee630ad7e0ff1b0e3f658cd0d3
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:04:55 +0500

    Add files via upload

---
+++ 23.conditional-test.ts
@@ -0,0 +1,50 @@
+// 1st statement
+let car = "supra";
+
+console.log("Is car == 'supra'? I predict True.")
+console.log(car == 'supra')
+
+console.log("Is car == 'toyota'? I predict false.")
+console.log(car == 'toyota');
+
+
+//2nd statement
+let city = "karachi";
+
+console.log("Is city == 'karachi'? I predict True.")
+console.log(city == 'karachi')
+
+console.log("Is city == 'lahore'? I predict false.")
+console.log(city == 'lahore');
+
+
+//3rd statement
+let country = "pakistan";
+
+console.log("Is country == 'pakistan'? I predict True.")
+console.log(country == 'pakistan')
+
+console.log("Is country == 'germany'? I predict false.")
+console.log(country == 'germany');
+
+
+
+//4th statement
+let fruit = "apple";
+
+console.log("Is fruit == 'apple'? I predict True.")
+console.log(fruit == 'apple');
+
+console.log("Is fruit == 'banana'? I predict false.")
+console.log(fruit == 'banana');
+
+
+
+//5th statement
+let number = "40";
+
+console.log("Is number == '40'? I predict True.")
+console.log(number == '40');
+
+console.log("Is number == '30'? I predict false.")
+console.log(number == '30');
