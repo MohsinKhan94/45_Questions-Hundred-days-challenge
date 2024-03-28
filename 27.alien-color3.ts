695331430d61aacef604d00a966cfd92f6108e72
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:18:59 +0500

    Add files via upload

---
+++ 27.alien-color3.ts
@@ -0,0 +1,36 @@
+// Green alien version:
+
+let alien_color = "green";
+
+if(alien_color == "green"){
+    console.log("you just earned 5 points")
+}else if(alien_color == "yellow"){
+    console.log("you just earned 10 points")
+}else if(alien_color == "red"){
+    console.log("you just earned 15 points")
+}
+
+// yellow alien version:
+
+alien_color = "yellow";
+
+if(alien_color == "green"){
+    console.log("you just earned 5 points")
+}else if(alien_color == "yellow"){
+    console.log("you just earned 10 points")
+}else if(alien_color == "red"){
+    console.log("you just earned 15 points")
+}
+
+
+// red alien version:
+
+alien_color = "red";
+
+if(alien_color == "green"){
+    console.log("you just earned 5 points")
+}else if(alien_color == "yellow"){
+    console.log("you just earned 10 points")
+}else if(alien_color == "red"){
+    console.log("you just earned 15 points")
+}
