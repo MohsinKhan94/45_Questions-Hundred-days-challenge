0c21f89410072560c087fd8e0b3e6cc47d51caf9
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:21:24 +0500

    Add files via upload

---
+++ 31.no-user.ts
@@ -0,0 +1,11 @@
+
+let usernames: string[] = ['admin', 'eric', 'john', 'alice', 'emma'];
+
+if(usernames.length === 0){
+    console.log("We need to find some users!")
+}else{
+    usernames = [];
+    console.log("All usernames have been removed " + usernames.length)
+}
+
+
