8a71af72125de7598e2653ebb39ac70e4db0ff5e
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:06:58 +0500

    Add files via upload

---
+++ 25.alien-color1.ts
@@ -0,0 +1,14 @@
+let alien_color = "green";
+
+// Version that passes:
+
+if (alien_color == "green") {
+    console.log("You just earned 5 points.")
+}
+
+// Version that fails (no output):
+
+alien_color = "green";
+if (alien_color == "red"){
+// No output because the condition is false
+}
