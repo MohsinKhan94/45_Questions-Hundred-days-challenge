2cff6ecaf190e3f6f55f746185e371a87ce94245
MohsinKhan94 <159677872+MohsinKhan94@users.noreply.github.com>
2024-03-28 18:04:14 +0500

    Add files via upload

---
+++ 22.international-error.ts
@@ -0,0 +1,11 @@
+
+
+let myFriends:string[] = ["mursaleen","ahmed","adnan","abdul rehman"];
+
+console.log(myFriends[4]); // the output is undefined
+
+// lets correct the error
+
+console.log(myFriends[2]); // Correcting the error by accessing a valid index.
+
+
